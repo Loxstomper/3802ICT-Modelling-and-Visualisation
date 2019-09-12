1fa4f5a4fbf11fe92796db3a2aa50654d1bc6f4c
loxstomper <lochieashcroft@gmail.com>
2019-09-12 13:23:28 +1000

    WIP: creating height map

--- assignment-2/part-1/visualisation/src/main.cpp
+++ assignment-2/part-1/visualisation/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include "headers/json.hpp"
 
@@ -13,18 +14,69 @@ Json fileToJson(std::string* filename) {
     return j;
 }
 
-int main(int argc, char** argv)
-{
-    if (argc < 2) {
-        std::cerr << "Provide a filename" << std::endl;
-        exit(1);
+void createHeightMap(Json* config, float** heightMap) {
+    int height = (*config)["height"].get<int>();
+    int width = (*config)["width"].get<int>();
+
+    std::cout << width << " " << height << std::endl;
+
+    // alloc space
+    heightMap = new float*[height];
+    for (int i = 0; i < height; i ++) {
+        heightMap[i] = new float[width];
     }
 
-    std::string filename = argv[1];
+    // copy the values in
+
+    for (int y = 0; y < height; y ++) {
+        std::vector<float> test = (*config)["heightMap"][y].get<std::vector<float>>();
+
+        // for (auto i = test.begin(); i != test.end(); ++i)
+        //     std::cout << *i << " ";
+
+        // std::cout << std::endl;
+
+        for (int x = 0; x < width; x ++) {
+            heightMap[y][x] = test[x];
+            // std::cout << heightMap[y][x] << std::endl;
+        }
+    }
+}
+
+void printHeightMap(int width, int height, float** heightMap) {
+    for (int y = 0; y < height; y ++) {
+        for (int x = 0; x < width; x ++) {
+            std::cout << heightMap[y][x] << " ";
+        }
+
+        std::cout << "\n";
+    }
+
+    std::cout << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+    // if (argc < 2) {
+    //     std::cerr << "Provide a filename" << std::endl;
+    //     exit(1);
+    // }
+
+    // std::string filename = argv[1];
+    std::string filename = "data.json";
 
     Json config = fileToJson(&filename);
 
-    std::cout << config << std::endl;
+    std::cout << "Input" << std::endl;
+    std::cout << std::setw(4) << config << std::endl;
+
+    float** heightMap;
+
+    createHeightMap(&config, heightMap);
+    // std::cout << heightMap[0][0] << std::endl;
+    // printHeightMap(4, 4, heightMap);
+    float a = heightMap[0][0];
+    a += 5;
 
     exit(0);
 }
