1d5961cee864608f598d998974c116dfe0d10a25
Lox <lox.ash@hotmail.com>
2019-09-18 20:47:04 +1000

    rotating perspective cube

--- assignment-2/part-1/learn-opengl/main.cpp
+++ assignment-2/part-1/learn-opengl/main.cpp
@@ -12,57 +12,122 @@
 #include <GL/glx.h>
 #include <GL/glext.h>
 
-#define SCREEN_HEIGHT 800
-#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT_DEFAULT 800
+#define SCREEN_WIDTH_DEFAULT 800
 #define WIREFRAME false
 #define FOV 60
 #define ASPECT_RATIO SCREEN_WIDTH / SCREEN_HEIGHT
 #define NEAR_CLIPPING_PLANE 1
 #define FAR_CLIPPING_PLANE 50000
 
-GLfloat HALF_SCREEN_HEIGHT = (GLfloat) SCREEN_HEIGHT / 2.0;
-GLfloat HALF_SCREEN_WIDTH = (GLfloat)SCREEN_WIDTH / 2.0;
+GLfloat SCREEN_WIDTH = SCREEN_WIDTH_DEFAULT;
+GLfloat SCREEN_HEIGHT = SCREEN_HEIGHT_DEFAULT;
+GLfloat HALF_SCREEN_HEIGHT = SCREEN_HEIGHT / 2.0;
+GLfloat HALF_SCREEN_WIDTH = SCREEN_WIDTH / 2.0;
 
+class Point3
+{
+public:
+    Point3(GLfloat x, GLfloat y, GLfloat z)
+    {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
 
-class Point3 {
-    public:
-        Point3(GLfloat x, GLfloat y, GLfloat z) 
-        {
-            this->x = x;
-            this->y = y;
-            this->z = z;
-        }
-
-        GLfloat x;
-        GLfloat y;
-        GLfloat z;
+    GLfloat x;
+    GLfloat y;
+    GLfloat z;
 };
 
 Point3 map[2][2] = {
-{Point3(0, 0, 0), Point3(1, 0, 0)},
-{Point3(0, 0, -1), Point3(1, 0, -1)}
-};
-
+    {Point3(0, 0, 0), Point3(1, 0, 0)},
+    {Point3(0, 0, -1), Point3(1, 0, -1)}};
 
 /* GLUT callback Handlers */
 static void onWindowResize(int width, int height)
 {
-    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-    glMatrixMode(GL_PROJECTION);
+    SCREEN_HEIGHT = height;
+    SCREEN_WIDTH = width;
+
+    // glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    // glMatrixMode(GL_PROJECTION);
+    // glLoadIdentity();
+    // glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
+    // glMatrixMode(GL_MODELVIEW);
+    // glLoadIdentity();
+
+    glMatrixMode(GL_PROJECTION_MATRIX);
     glLoadIdentity();
-    glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity;
+    gluPerspective(60, (double)SCREEN_WIDTH / (double)SCREEN_HEIGHT, 0.1, 1000);
+
+    glMatrixMode(GL_MODELVIEW_MATRIX);
+    glTranslatef(0, 0, -5);
+}
+
+void drawCube()
+{
+    GLfloat vertices[] =
+        {
+            -1, -1, -1, -1, -1, 1, -1, 1, 1, -1, 1, -1,
+            1, -1, -1, 1, -1, 1, 1, 1, 1, 1, 1, -1,
+            -1, -1, -1, -1, -1, 1, 1, -1, 1, 1, -1, -1,
+            -1, 1, -1, -1, 1, 1, 1, 1, 1, 1, 1, -1,
+            -1, -1, -1, -1, 1, -1, 1, 1, -1, 1, -1, -1,
+            -1, -1, 1, -1, 1, 1, 1, 1, 1, 1, -1, 1};
+
+    GLfloat colors[] =
+        {
+            0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0,
+            1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0,
+            0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0,
+            0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0,
+            0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0,
+            0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1};
+
+    static float alpha = 0;
+    //attempt to rotate cube
+    glRotatef(alpha, 0, 1, 0);
+
+    /* We have a color array and a vertex array */
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+    glVertexPointer(3, GL_FLOAT, 0, vertices);
+    glColorPointer(3, GL_FLOAT, 0, colors);
+
+    /* Send data : 24 vertices */
+    glDrawArrays(GL_QUADS, 0, 24);
+
+    /* Cleanup states */
+    glDisableClientState(GL_COLOR_ARRAY);
+    glDisableClientState(GL_VERTEX_ARRAY);
+    alpha += 1;
 }
 
 static void render(void)
 {
-    glClearColor(255, 253, 208, 100);
+    // glClearColor(255, 253, 208, 100);
+    // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Draw stuff
+    glClearColor(0.0, 0.8, 0.3, 1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION_MATRIX);
+    glLoadIdentity();
+    gluPerspective(60, (double)SCREEN_WIDTH / (double)SCREEN_HEIGHT, 0.1, 100);
+
+    glMatrixMode(GL_MODELVIEW_MATRIX);
+    glTranslatef(0, 0, -5);
+
+    drawCube();
+
+    glutSwapBuffers();
+
+    return;
 
     glPushMatrix();
     // glTranslatef(HALF_SCREEN_WIDTH, HALF_SCREEN_HEIGHT, -500);
-
 
     // glLoadIdentity();
 
@@ -70,21 +135,19 @@ static void render(void)
     //     -0.5f, -0.5f, 0.0f,
     //     0.5f, -0.5f, 0.0f,
     //     0.0f,  0.5f, 0.0f
-    // }; 
+    // };
 
     GLfloat vertices[] = {
         -0.5f, -0.5f, 0.0f,
         -0.5f, 0.5f, 0.0f,
         // 0.5f, 0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
-    }; 
+        0.0f, 0.5f, 0.0f};
 
     GLfloat colours[] = {
         1.0, 0, 0, 1.0,
-        0, 1.0, 0, 1.0, 
+        0, 1.0, 0, 1.0,
         0, 0, 1.0, 1.0,
-        1.0, 1.0, 0, 1.0
-    };
+        1.0, 1.0, 0, 1.0};
 
     glEnableClientState(GL_VERTEX_ARRAY);
     glEnableClientState(GL_COLOR_ARRAY);
@@ -93,11 +156,9 @@ static void render(void)
     // glVertexPointer(4, GL_FLOAT, 0, vertices);
     glColorPointer(4, GL_FLOAT, 0, colours);
 
-
     // glDrawArrays(GL_TRIANGLES, 0, 3);
     // glDrawArrays(GL_QUADS, 0, 3);
 
-
     // GLfloat vertices2[] = {-0.5, -0.5, 0, // bottom left corner
     //                   -0.5,  0.5, 0, // top left corner
     //                    0.5,  0.5, 0, // top right corner
@@ -107,13 +168,13 @@ static void render(void)
     //                   -0.5,  0.5, 0, // top left corner
     //                    0.5,  0.5, 0, // top right corner
     //                    0.5, -0.5, 0}; // bottom right corner
-    GLfloat vertices2[] = {0, 0, 0, // bottom left corner
-                       0,  100, 0, // top left corner
-                       100,  100, 0, // top right corner
-                       100, -0, 0}; // bottom right corner
+    GLfloat vertices2[] = {0, 0, 0,     // bottom left corner
+                           0, 100, 0,   // top left corner
+                           100, 100, 0, // top right corner
+                           100, -0, 0}; // bottom right corner
 
-    GLubyte indices[] = {0,1,2, // first triangle (bottom left - top left - top right)
-                     0,2,3}; // second triangle (bottom left - top right - bottom right)
+    GLubyte indices[] = {0, 1, 2,  // first triangle (bottom left - top left - top right)
+                         0, 2, 3}; // second triangle (bottom left - top right - bottom right)
 
     glVertexPointer(3, GL_FLOAT, 0, vertices2);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, indices);
@@ -122,7 +183,6 @@ static void render(void)
     glDisableClientState(GL_VERTEX_ARRAY);
 
     glutSwapBuffers();
-
 }
 
 static void onSpecialKey(int key, int x, int y)
@@ -165,13 +225,17 @@ void glutSetup(int *argc, char **argv)
     glutPassiveMotionFunc(onMouseMovement); // mouse movement no buttons
     glutIdleFunc(idle);
 
+    glEnable(GL_DEPTH_TEST); // Depth Testing
+    glDepthFunc(GL_LEQUAL);
+    glDisable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+
     glutMainLoop();
 }
 
-int main(int argc, char** argv) 
+int main(int argc, char **argv)
 {
     glutSetup(&argc, argv);
 
     return EXIT_SUCCESS;
 }
-
