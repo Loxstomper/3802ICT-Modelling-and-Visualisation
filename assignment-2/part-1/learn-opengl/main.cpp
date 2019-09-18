2ab186570915b8a497d9eb1ff64f026e6b5003c3
loxstomper <lochieashcroft@gmail.com>
2019-09-18 20:27:20 +1000

    learning open-gl

---
+++ assignment-2/part-1/learn-opengl/main.cpp
@@ -0,0 +1,177 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+
+#include <GL/glut.h>
+#include <GL/gl.h>
+
+#define GL_GLEXT_PROTOTYPES
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glx.h>
+#include <GL/glext.h>
+
+#define SCREEN_HEIGHT 800
+#define SCREEN_WIDTH 800
+#define WIREFRAME false
+#define FOV 60
+#define ASPECT_RATIO SCREEN_WIDTH / SCREEN_HEIGHT
+#define NEAR_CLIPPING_PLANE 1
+#define FAR_CLIPPING_PLANE 50000
+
+GLfloat HALF_SCREEN_HEIGHT = (GLfloat) SCREEN_HEIGHT / 2.0;
+GLfloat HALF_SCREEN_WIDTH = (GLfloat)SCREEN_WIDTH / 2.0;
+
+
+class Point3 {
+    public:
+        Point3(GLfloat x, GLfloat y, GLfloat z) 
+        {
+            this->x = x;
+            this->y = y;
+            this->z = z;
+        }
+
+        GLfloat x;
+        GLfloat y;
+        GLfloat z;
+};
+
+Point3 map[2][2] = {
+{Point3(0, 0, 0), Point3(1, 0, 0)},
+{Point3(0, 0, -1), Point3(1, 0, -1)}
+};
+
+
+/* GLUT callback Handlers */
+static void onWindowResize(int width, int height)
+{
+    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity;
+}
+
+static void render(void)
+{
+    glClearColor(255, 253, 208, 100);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glPushMatrix();
+    // glTranslatef(HALF_SCREEN_WIDTH, HALF_SCREEN_HEIGHT, -500);
+
+
+    // glLoadIdentity();
+
+    // GLfloat vertices[] = {
+    //     -0.5f, -0.5f, 0.0f,
+    //     0.5f, -0.5f, 0.0f,
+    //     0.0f,  0.5f, 0.0f
+    // }; 
+
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f,
+        // 0.5f, 0.5f, 0.0f,
+        0.0f,  0.5f, 0.0f
+    }; 
+
+    GLfloat colours[] = {
+        1.0, 0, 0, 1.0,
+        0, 1.0, 0, 1.0, 
+        0, 0, 1.0, 1.0,
+        1.0, 1.0, 0, 1.0
+    };
+
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+
+    // glVertexPointer(3, GL_FLOAT, 0, vertices);
+    // glVertexPointer(4, GL_FLOAT, 0, vertices);
+    glColorPointer(4, GL_FLOAT, 0, colours);
+
+
+    // glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glDrawArrays(GL_QUADS, 0, 3);
+
+
+    // GLfloat vertices2[] = {-0.5, -0.5, 0, // bottom left corner
+    //                   -0.5,  0.5, 0, // top left corner
+    //                    0.5,  0.5, 0, // top right corner
+    //                    0.5, -0.5, 0}; // bottom right corner
+
+    // GLfloat vertices2[] = {-0.5, -0.5, 0, // bottom left corner
+    //                   -0.5,  0.5, 0, // top left corner
+    //                    0.5,  0.5, 0, // top right corner
+    //                    0.5, -0.5, 0}; // bottom right corner
+    GLfloat vertices2[] = {0, 0, 0, // bottom left corner
+                       0,  100, 0, // top left corner
+                       100,  100, 0, // top right corner
+                       100, -0, 0}; // bottom right corner
+
+    GLubyte indices[] = {0,1,2, // first triangle (bottom left - top left - top right)
+                     0,2,3}; // second triangle (bottom left - top right - bottom right)
+
+    glVertexPointer(3, GL_FLOAT, 0, vertices2);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, indices);
+
+    glEnableClientState(GL_COLOR_ARRAY);
+    glDisableClientState(GL_VERTEX_ARRAY);
+
+    glutSwapBuffers();
+
+}
+
+static void onSpecialKey(int key, int x, int y)
+{
+}
+
+static void onKey(unsigned char key, int x, int y)
+{
+    glutPostRedisplay();
+}
+
+static void onMouse(int button, int state, int x, int y)
+{
+}
+
+void onMouseMovement(int x, int y)
+{
+}
+
+static void idle(void)
+{
+    glutPostRedisplay();
+}
+
+void glutSetup(int *argc, char **argv)
+{
+    glutInit(argc, argv);
+    glutInitWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    glutInitWindowPosition(100, 100);
+    // glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+
+    glutCreateWindow("Terrain Visualisation");
+
+    glutReshapeFunc(onWindowResize);
+    glutDisplayFunc(render);
+    glutKeyboardFunc(onKey);
+    glutSpecialFunc(onSpecialKey);
+    glutMouseFunc(onMouse);
+    glutPassiveMotionFunc(onMouseMovement); // mouse movement no buttons
+    glutIdleFunc(idle);
+
+    glutMainLoop();
+}
+
+int main(int argc, char** argv) 
+{
+    glutSetup(&argc, argv);
+
+    return EXIT_SUCCESS;
+}
+
