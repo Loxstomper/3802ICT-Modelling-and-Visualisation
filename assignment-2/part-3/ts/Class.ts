894f9829485bef55e1894484a945a1d30f5e7cfd
Lox <lox.ash@hotmail.com>
2019-09-22 11:43:03 +1000

    WIP: parsing

--- assignment-2/part-3/ts/Class.ts
+++ assignment-2/part-3/ts/Class.ts
@@ -1,14 +1,13 @@
 export class Class {
   public name: string;
   public parent?: string | Class;
-  public children?: string[] | Class[];
+  public children?: Class[];
   public properties?: string[];
   public methods?: string[];
 
   constructor(name: string, parent?: string) {
     this.name = name;
     this.parent = parent;
-
   }
 
   public getWidth(): number {
--- assignment-2/part-3/ts/main.ts
+++ assignment-2/part-3/ts/main.ts
@@ -1,4 +1,4 @@
-import {Class} from "./Class";
+import { Class } from "./Class";
 
 let UMLInputString = `
 class Banana extends Apple
@@ -31,99 +31,118 @@ class Kiwifruit extends Cherry
 `;
 
 // maybe change to const
-let classes: Class[] = [];
+const classes: Class[] = [];
 
-function setup() : void {
-    (<HTMLInputElement>document.getElementById("draw")).onclick = (e: MouseEvent) => {
-        UMLInputString = (<HTMLInputElement> document.getElementById("UML-input")).value;
-        console.log(UMLInputString);
-    }
+function setup(): void {
+  (document.getElementById("draw") as HTMLInputElement).onclick = (
+    e: MouseEvent
+  ) => {
+    UMLInputString = (document.getElementById("UML-input") as HTMLInputElement)
+      .value;
+    console.log(UMLInputString);
+  };
 
-    read();
+  // wipe without destroying reference
+  classes.length = 0;
+
+  read();
 }
 
-function inClasses(name: string) : number {
-    for (let i : number = 0; i < classes.length; i ++) {
-        if (classes[i].name === name) {
-            return i;
-        }
+function inClasses(name: string): number {
+  for (let i: number = 0; i < classes.length; i++) {
+    if (classes[i].name === name) {
+      return i;
     }
+  }
 
-    return -1;
+  return -1;
 }
 
 function read(): void {
-    console.log('reading');
+  console.log("reading");
 
-    const lines: string [] = UMLInputString.split('\n');
+  const lines: string[] = UMLInputString.split("\n");
 
-    let currentClass: string;
+  let currentClass: string;
 
-    for (let i = 0; i < lines.length; i ++) {
-        const tokens: string[] = lines[i].split(' ');
+  for (let i = 0; i < lines.length; i++) {
+    const tokens: string[] = lines[i].split(" ");
 
-        if (tokens[0] === "class") {
+    if (tokens[0] === "class") {
+      // let cLocation = inClasses(tokens[0]);
 
-            // let cLocation = inClasses(tokens[0]);
+      // if (cLocation !== -1) {
 
-            // if (cLocation !== -1) {
+      // }
 
-            // }
+      const c: Class =
+        tokens.length === 4
+          ? new Class(tokens[1], tokens[3])
+          : new Class(tokens[1]);
 
+      let parent: Class = null;
 
-            let c: Class = tokens.length === 4 ? new Class(tokens[1], tokens[3]) : new Class(tokens[1]);
-
-            for (let j = i + 1 ; j < lines.length; j ++) {
-
-                if (lines[j].startsWith('class') || lines[j].startsWith(' ')) {
-                // if (lines[j].startsWith('class')) {
-                    break;
-                }
-
-                // console.log(lines[j]);
-
-                // console.log(lines[j]);
-
-                if (lines[j].includes('(')) {
-                    if (!c.methods) {
-                        c.methods = [];
-                    }
-
-                    c.methods.push(lines[j]);
-                } else {
-                    if (!c.properties) {
-                        c.properties = [];
-                    }
-                    // c.properties.push(lines[i + 1]);
-                    c.properties.push(lines[j]);
-                }
-            }
-
-            // console.log(c);
-            // return;
-
-            // console.log(c);
-            classes.push(c);
-
-            // console.log(c);
-
-            // console.log(tokens[1]);
+      for (let j = i + 1; j < lines.length; j++) {
+        if (lines[j].startsWith("class") || lines[j].startsWith(" ")) {
+          break;
         }
 
+        // check if already exists (children created it)
+        // only need to update the details
+        const alreadyExists = inClasses(tokens[1]) !== -1;
 
+        if (alreadyExists) {
+          console.log("already created");
+          continue;
+        }
 
+        // create parent if not exists
+        if (tokens.length === 4) {
+          if (inClasses(tokens[3]) === -1) {
+            classes.push(new Class(tokens[3]));
+            parent = classes[classes.length - 1];
+            parent.children = [];
+          }
+        }
 
+        // console.log(lines[j]);
+
+        // console.log(lines[j]);
+
+        // methods
+        if (lines[j].includes("(")) {
+          if (!c.methods) {
+            c.methods = [];
+          }
+
+          c.methods.push(lines[j]);
+          // properties
+        } else if (lines[j].includes(":")) {
+          if (!c.properties) {
+            c.properties = [];
+          }
+          // c.properties.push(lines[i + 1]);
+          c.properties.push(lines[j]);
+        }
+      }
+
+      // console.log(c);
+      // return;
+
+      // is a child
+      if (parent) {
+        c.parent = parent;
+        parent.children.push(c);
+        // is a parent
+      } else {
+        classes.push(c);
+      }
     }
+  }
 
-    console.log(classes);
+  console.log(classes);
 }
 
 setup();
 
-
-
-
-
-
 const x: Class = new Class("Banana", "Apple");
-
