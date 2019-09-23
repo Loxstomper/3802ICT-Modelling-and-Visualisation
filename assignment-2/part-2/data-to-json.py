0833f23aad728f74d074dcd168d0e42f21492c09
Lox <lox.ash@hotmail.com>
2019-09-23 21:01:07 +1000

    created json

---
+++ assignment-2/part-2/data-to-json.py
@@ -0,0 +1,30 @@
+#! /bin/python3
+
+import json
+import csv
+
+data = {
+    "dates": [],
+    "15-24": [],
+    "25-34": [],
+    "35-44": [],
+    "45-54": [],
+    "55+": []
+}
+
+with open ("./data/filtered.csv") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+
+    for row in csv_reader:
+        # header
+        if row[0] == "Month":
+            continue
+
+        data["dates"].append(row[0])
+        data["15-24"].append(row[1])
+        data["25-34"].append(row[2])
+        data["35-44"].append(row[3])
+        data["45-54"].append(row[4])
+        data["55+"].append(row[5])
+
+print(json.dumps(data, indent=4))
