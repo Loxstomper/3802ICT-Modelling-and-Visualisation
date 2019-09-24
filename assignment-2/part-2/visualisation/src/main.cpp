3b87de298609f64a648733aad47c7bf7c8ce9ddf
Lox <lox.ash@hotmail.com>
2019-09-24 21:37:46 +1000

    wip: series rendering

--- assignment-2/part-2/visualisation/src/main.cpp
+++ assignment-2/part-2/visualisation/src/main.cpp
@@ -27,7 +27,7 @@ using Json = nlohmann::json;
 #define GRAPH_WIDTH 1.0
 #define GRAPH_LENGTH 1.0
 #define GRAPH_HEIGHT 1.0
-#define GRAPH_WALL_THICKNESS 0.25
+#define GRAPH_WALL_THICKNESS -0.25
 
 GLfloat SCREEN_WIDTH = SCREEN_WIDTH_DEFAULT;
 GLfloat SCREEN_HEIGHT = SCREEN_HEIGHT_DEFAULT;
@@ -261,6 +261,134 @@ void drawCube()
     glDisableClientState(GL_VERTEX_ARRAY);
 }
 
+void drawSeries(double *series, double z, GLfloat height)
+{
+
+    GLfloat xStep = graphData.length / (GRAPH_LENGTH * 2);
+    // GLfloat seriesThickness = graphData.length / (GRAPH_WIDTH * 2);
+    GLfloat seriesThickness = (GRAPH_WIDTH * 2) / graphData.length;
+    GLfloat curX = -1;
+    int vi = 0;
+
+    // this will be the series value
+    // GLfloat height = 1.0;
+
+    GLfloat myVertices[] = {
+
+        // front facing
+        -1,
+        0,
+        z,
+        1,
+        0,
+        z,
+        1,
+        height,
+        z,
+        -1,
+        height,
+        z,
+
+        // rear side [not really required]
+        -1,
+        0,
+        z + seriesThickness,
+        1,
+        0,
+        z + seriesThickness,
+        1,
+        height,
+        z + seriesThickness,
+        -1,
+        height,
+        z + seriesThickness,
+
+        // top side
+        -1,
+        height,
+        z,
+        1,
+        height,
+        z,
+        1,
+        height,
+        z + seriesThickness,
+        -1,
+        height,
+        z + seriesThickness,
+
+        // left side
+        -1,
+        0,
+        z,
+        -1,
+        0,
+        z + seriesThickness,
+        -1,
+        height,
+        z + seriesThickness,
+        -1,
+        height,
+        z,
+
+        // right side
+        1,
+        0,
+        z,
+        1,
+        0,
+        z + seriesThickness,
+        1,
+        height,
+        z + seriesThickness,
+        1,
+        height,
+        z};
+
+    for (int i = 0; i < graphData.length; i++)
+    {
+
+        curX += xStep;
+        vi += 3;
+    }
+
+    GLfloat vertices[] =
+        {
+            -1, -1, -1, -1, -1, 1, -1, 1, 1, -1, 1, -1,
+            1, -1, -1, 1, -1, 1, 1, 1, 1, 1, 1, -1,
+            -1, -1, -1, -1, -1, 1, 1, -1, 1, 1, -1, -1,
+            -1, 1, -1, -1, 1, 1, 1, 1, 1, 1, 1, -1,
+            -1, -1, -1, -1, 1, -1, 1, 1, -1, 1, -1, -1,
+            -1, -1, 1, -1, 1, 1, 1, 1, 1, 1, -1, 1};
+
+    GLfloat colors[] =
+        {
+            0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0,
+            1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0,
+            0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0,
+            0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0,
+            0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0,
+            0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1};
+
+    //attempt to rotate cube
+    // glRotatef(alpha, 0, 1, 0);
+
+    /* We have a color array and a vertex array */
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+    // glVertexPointer(3, GL_FLOAT, 0, vertices);
+    glVertexPointer(3, GL_FLOAT, 0, myVertices);
+    glColorPointer(3, GL_FLOAT, 0, colors);
+
+    /* Send data : 24 vertices */
+    // glDrawArrays(GL_QUADS, 0, 24);
+    glDrawArrays(GL_QUADS, 0, 20);
+
+    /* Cleanup states */
+    glDisableClientState(GL_COLOR_ARRAY);
+    glDisableClientState(GL_VERTEX_ARRAY);
+}
+
 static void render(void)
 {
     glClearColor(255, 253, 208, 100);
@@ -284,6 +412,12 @@ static void render(void)
 
     // normal mode
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+    drawSeries(graphData.series0, -1, 0.5);
+    drawSeries(graphData.series0, -0.6, 1.0);
+    drawSeries(graphData.series0, -0.2, 0.5);
+    drawSeries(graphData.series0, 0.2, 1.0);
+    drawSeries(graphData.series0, 0.6, 0.5);
 
     alpha++;
 
@@ -370,6 +504,8 @@ void readData(std::string filename)
 
     file >> json;
 
+    std::cout << json << std::endl;
+
     // get data length
     graphData.length = json["length"].get<GLfloat>();
 
@@ -389,9 +525,23 @@ void readData(std::string filename)
     fillSeriesData(json["15-24"].get<std::vector<double>>(), graphData.series0);
 }
 
+void readDataTemp()
+{
+    graphData.length = 5;
+    graphData.series0 = new double[5];
+
+    // [0.1, 0.2, 0.3, 0.4, 0.5]
+    for (int i = 1; i < graphData.length + 1; i++)
+    {
+        graphData.series0[i - 1] = i * 0.1;
+        std::cout << graphData.series0[i - 1] << std::endl;
+    }
+}
+
 int main(int argc, char **argv)
 {
-    readData("./data/data.json");
+    // readData("./data/data.json");
+    readDataTemp();
     glutSetup(&argc, argv);
 
     return EXIT_SUCCESS;
