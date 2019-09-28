5298f95da91502e6f54a26b067468e4f26e38012
Lox <lox.ash@hotmail.com>
2019-09-28 22:52:36 +1000

    cheesed series drawing

--- assignment-2/part-2/visualisation/src/main.cpp
+++ assignment-2/part-2/visualisation/src/main.cpp
@@ -39,6 +39,10 @@ GLfloat CURRENT_Z_DEPTH = -5;
 GLfloat alpha = 0;
 bool alphaLock = false;
 
+// what to scale the values by [-1, 1]
+GLfloat largestValue = -1;
+GLfloat smallestValue = 999;
+
 typedef struct data Data;
 struct data
 {
@@ -47,16 +51,21 @@ struct data
     // std::string *dates;
     std::string *dates;
     std::string *seriesNames;
-    double *series0;
-    double *series1;
-    double *series2;
-    double *series3;
-    double *series4;
-    double *series5;
-    double *series6;
+    GLfloat *series0;
+    GLfloat *series1;
+    GLfloat *series2;
+    GLfloat *series3;
+    GLfloat *series4;
+    GLfloat *series5;
+    GLfloat *series6;
 };
 
 Data graphData;
+
+GLfloat scale(GLfloat value, GLfloat min, GLfloat max)
+{
+    return (value - min) / (max - min);
+}
 
 // void drawString(char *string, float x, float y, float z)
 void drawString(std::string string, float x, float y, float z, float xRot, float yRot, float zRot)
@@ -322,6 +331,134 @@ void drawSeries(double *series, GLfloat z, GLfloat height, GLfloat colour[])
     glDisableClientState(GL_VERTEX_ARRAY);
 }
 
+void drawSeriesBad(GLfloat *series, GLfloat z, GLfloat colour[])
+{
+    GLfloat xStep = (2 * GRAPH_LENGTH) / graphData.length;
+    // GLfloat seriesThickness = graphData.length / (GRAPH_WIDTH * 2);
+    GLfloat seriesThickness = (GRAPH_WIDTH * 2) / graphData.numberSeries;
+    GLfloat curX = -1;
+    int vi = 0;
+
+    GLfloat low = -1.0;
+
+    GLfloat padding = 0.1;
+
+    GLfloat black[] = {0, 0, 0, 0.5};
+
+    z -= padding;
+    seriesThickness -= padding;
+    // set colour
+    glColor4fv(colour);
+
+    // front
+    glColor4fv(colour);
+    glBegin(GL_POLYGON);
+    glVertex3f(curX, low, z);
+    for (int i = 0; i < graphData.length; i++)
+    {
+        glVertex3f(curX, scale(series[i], smallestValue, largestValue), z);
+        curX += xStep;
+    }
+    glVertex3f(curX, low, z);
+    glEnd();
+
+    // back
+    curX = -1;
+    glBegin(GL_POLYGON);
+    glVertex3f(curX, low, z + seriesThickness);
+    for (int i = 0; i < graphData.length; i++)
+    {
+        glVertex3f(curX, scale(series[i], smallestValue, largestValue), z + seriesThickness);
+        curX += xStep;
+    }
+    glVertex3f(curX, low, z + seriesThickness);
+    glEnd();
+
+    // side 1
+    glBegin(GL_QUADS);
+    glVertex3f(curX, low, z);
+    glVertex3f(curX, low, z + seriesThickness);
+    glVertex3f(curX, scale(series[graphData.length - 1], smallestValue, largestValue), z + seriesThickness);
+    glVertex3f(curX, scale(series[graphData.length - 1], smallestValue, largestValue), z);
+    glEnd();
+
+    // side 2
+    curX = -1;
+    glBegin(GL_QUADS);
+    glVertex3f(-1, low, z);
+    glVertex3f(-1, low, z + seriesThickness);
+    glVertex3f(-1, scale(series[0], smallestValue, largestValue), z + seriesThickness);
+    glVertex3f(-1, scale(series[0], smallestValue, largestValue), z);
+    glEnd();
+
+    // tops
+    curX = -1;
+    glBegin(GL_QUADS);
+    for (int i = 0; i < graphData.length - 1; i++)
+    {
+        glVertex3f(curX, scale(series[i], smallestValue, largestValue), z);
+        glVertex3f(curX, scale(series[i], smallestValue, largestValue), z + seriesThickness);
+
+        curX += xStep;
+
+        glVertex3f(curX, scale(series[i + 1], smallestValue, largestValue), z);
+        glVertex3f(curX, scale(series[i + 1], smallestValue, largestValue), z + seriesThickness);
+    }
+
+    glEnd();
+
+    // drawing outline - had issues with wireframe
+    glColor3fv(black);
+    curX = -1;
+    glBegin(GL_LINE_LOOP);
+    glVertex3f(curX, low, z);
+    for (int i = 0; i < graphData.length; i++)
+    {
+        glVertex3f(curX, scale(series[i], smallestValue, largestValue), z);
+        curX += xStep;
+    }
+    glVertex3f(curX, low, z);
+    curX = -1;
+    glEnd();
+
+    glColor3fv(black);
+    curX = -1;
+    glBegin(GL_LINE_LOOP);
+    glVertex3f(curX, low, z + seriesThickness);
+    for (int i = 0; i < graphData.length; i++)
+    {
+        glVertex3f(curX, scale(series[i], smallestValue, largestValue), z + seriesThickness);
+        curX += xStep;
+    }
+    glVertex3f(curX, low, z + seriesThickness);
+    curX = -1;
+    glEnd();
+
+    glBegin(GL_LINES);
+    glVertex3f(curX, low, z);
+    glVertex3f(curX, low, z + seriesThickness);
+
+    curX = -1;
+
+    glVertex3f(curX, low, z);
+    glVertex3f(curX, low, z + seriesThickness);
+
+    glEnd();
+
+    glBegin(GL_LINES);
+
+    curX = -1;
+
+    for (int i = 0; i < graphData.length; i++)
+    {
+        glVertex3f(curX, scale(series[i], smallestValue, largestValue), z);
+        glVertex3f(curX, scale(series[i], smallestValue, largestValue), z + seriesThickness);
+        curX += xStep;
+    }
+
+    glEnd();
+}
+
 void drawSeriesLabels()
 {
     GLfloat x = -1.4;
@@ -367,12 +504,12 @@ static void render(void)
 {
     GLfloat black[] = {0, 0, 0, 1};
 
-    GLfloat yellow[] = {1, 1, 0, 1};
-    GLfloat purple[] = {0.5, 0, 0.5, 1};
-    GLfloat orange[] = {1, 0.5, 0, 1};
-    GLfloat green[] = {0, 0, 1, 1};
-    GLfloat red[] = {1, 0, 0, 1};
-    GLfloat blue[] = {0, 1, 0, 1};
+    GLfloat yellow[] = {1, 1, 0, 0.5};
+    GLfloat purple[] = {0.5, 0, 0.5, 0.5};
+    GLfloat orange[] = {1, 0.5, 0, 0.5};
+    GLfloat green[] = {0, 0, 1, 0.5};
+    GLfloat red[] = {1, 0, 0, 0.5};
+    GLfloat blue[] = {0, 1, 0, 0.5};
 
     if (alphaLock)
     {
@@ -415,19 +552,19 @@ static void render(void)
     GLfloat z = -1;
     GLfloat zStep = (2 / GLfloat(graphData.numberSeries));
 
-    drawSeries(graphData.series6, z, 0.5, blue);
+    drawSeriesBad(graphData.series6, z, blue);
     z += zStep;
-    drawSeries(graphData.series5, z, 1.0, red);
+    drawSeriesBad(graphData.series5, z, red);
     z += zStep;
-    drawSeries(graphData.series4, z, 0.5, yellow);
+    drawSeriesBad(graphData.series4, z, yellow);
     z += zStep;
-    drawSeries(graphData.series3, z, 1.0, green);
+    drawSeriesBad(graphData.series3, z, green);
     z += zStep;
-    drawSeries(graphData.series2, z, 0.5, yellow);
+    drawSeriesBad(graphData.series2, z, yellow);
     z += zStep;
-    drawSeries(graphData.series1, z, 0.5, blue);
+    drawSeriesBad(graphData.series1, z, blue);
     z += zStep;
-    drawSeries(graphData.series0, z, 0.5, red);
+    drawSeriesBad(graphData.series0, z, red);
 
     alpha++;
 
@@ -498,17 +635,26 @@ void glutSetup(int *argc, char **argv)
     glDisable(GL_CULL_FACE);
     glCullFace(GL_BACK);
 
+    // transparency
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
     glutMainLoop();
 }
 
-void fillSeriesData(std::vector<double> input, double *out)
+void fillSeriesData(std::vector<GLfloat> input, GLfloat *out)
 {
     for (int i = 0; i < input.size(); i++)
     {
+        if (input[i] > largestValue)
+            largestValue = input[i];
+
+        if (input[i] < smallestValue)
+            smallestValue = input[i];
+
         out[i] = input[i];
     }
 }
-
 void readData(std::string filename)
 {
     std::ifstream file(filename);
@@ -541,50 +687,22 @@ void readData(std::string filename)
     graphData.seriesNames[5] = "45-54";
     graphData.seriesNames[6] = "55 and over";
 
-    graphData.series0 = new double[graphData.length];
-    graphData.series1 = new double[graphData.length];
-    graphData.series2 = new double[graphData.length];
-    graphData.series3 = new double[graphData.length];
-    graphData.series4 = new double[graphData.length];
-    graphData.series5 = new double[graphData.length];
-    graphData.series6 = new double[graphData.length];
+    graphData.series0 = new GLfloat[graphData.length];
+    graphData.series1 = new GLfloat[graphData.length];
+    graphData.series2 = new GLfloat[graphData.length];
+    graphData.series3 = new GLfloat[graphData.length];
+    graphData.series4 = new GLfloat[graphData.length];
+    graphData.series5 = new GLfloat[graphData.length];
+    graphData.series6 = new GLfloat[graphData.length];
 
     // read the data
-    fillSeriesData(json["15-64"].get<std::vector<double>>(), graphData.series0);
-    fillSeriesData(json["15-24"].get<std::vector<double>>(), graphData.series1);
-    fillSeriesData(json["15-19"].get<std::vector<double>>(), graphData.series2);
-    fillSeriesData(json["25-34"].get<std::vector<double>>(), graphData.series3);
-    fillSeriesData(json["35-44"].get<std::vector<double>>(), graphData.series4);
-    fillSeriesData(json["45-54"].get<std::vector<double>>(), graphData.series5);
-    fillSeriesData(json["55 and over"].get<std::vector<double>>(), graphData.series6);
-}
-
-void readDataTemp()
-{
-    graphData.length = 5;
-    graphData.numberSeries = 5;
-    graphData.series0 = new double[5];
-
-    graphData.seriesNames = new std::string[5];
-
-    graphData.seriesNames[0] = "data0";
-    graphData.seriesNames[1] = "data1";
-    graphData.seriesNames[2] = "data2";
-    graphData.seriesNames[3] = "data3";
-    graphData.seriesNames[4] = "data4";
-
-    graphData.dates = new std::string[5];
-    graphData.dates[0] = "1901";
-    graphData.dates[1] = "1902";
-    graphData.dates[2] = "1903";
-    graphData.dates[3] = "1904";
-    graphData.dates[4] = "1905";
-
-    // [0.1, 0.2, 0.3, 0.4, 0.5]
-    for (int i = 1; i < graphData.length + 1; i++)
-    {
-        graphData.series0[i - 1] = i * 0.1;
-    }
+    fillSeriesData(json["15-64"].get<std::vector<GLfloat>>(), graphData.series0);
+    fillSeriesData(json["15-24"].get<std::vector<GLfloat>>(), graphData.series1);
+    fillSeriesData(json["15-19"].get<std::vector<GLfloat>>(), graphData.series2);
+    fillSeriesData(json["25-34"].get<std::vector<GLfloat>>(), graphData.series3);
+    fillSeriesData(json["35-44"].get<std::vector<GLfloat>>(), graphData.series4);
+    fillSeriesData(json["45-54"].get<std::vector<GLfloat>>(), graphData.series5);
+    fillSeriesData(json["55 and over"].get<std::vector<GLfloat>>(), graphData.series6);
 }
 
 int main(int argc, char **argv)
