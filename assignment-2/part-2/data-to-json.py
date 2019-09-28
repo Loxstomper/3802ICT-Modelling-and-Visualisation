4f24b5445e01a73e0e9fdf9c9db3bd5a1ca617e5
Lox <lox.ash@hotmail.com>
2019-09-28 21:00:06 +1000

    updated data

--- assignment-2/part-2/data-to-json.py
+++ assignment-2/part-2/data-to-json.py
@@ -5,30 +5,34 @@ import csv
 
 data = {
     "length": 0,
-    "dates": [],
+    "Years": [],
+    "15-64": [],
     "15-24": [],
+    "15-19": [],
     "25-34": [],
     "35-44": [],
     "45-54": [],
-    "55+": []
+    "55 and over": []
 }
 
-with open ("./data/filtered.csv") as csv_file:
+with open ("./data/years.csv") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
 
     length = 0
 
     for row in csv_reader:
         # header
-        if row[0] == "Month":
+        if row[0] == "Year":
             continue
 
-        data["dates"].append(row[0])
-        data["15-24"].append(row[1])
-        data["25-34"].append(row[2])
-        data["35-44"].append(row[3])
-        data["45-54"].append(row[4])
-        data["55+"].append(row[5])
+        data["Years"].append(row[0])
+        data["15-64"].append(row[1])
+        data["15-24"].append(row[2])
+        data["15-19"].append(row[3])
+        data["25-34"].append(row[4])
+        data["35-44"].append(row[5])
+        data["45-54"].append(row[6])
+        data["55 and over"].append(row[7])
         length += 1
 
     data["length"] = length
